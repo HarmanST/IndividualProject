a8daf758b66cfc1a7613dbbca915d3ff66f12061
HarmanST <harman.tathgar@gmail.com>
2024-08-23 14:23:02 +0100

    Clean up code
    created a js script for index.html
    Also removed css from from index.html

---
+++ static/script.js
@@ -0,0 +1,43 @@
+var map;
+var mapModal = document.getElementById('map-modal');
+var useMapButton = document.getElementById('use-map-button');
+var closeMapButton = document.getElementById('close-map');
+
+useMapButton.onclick = function() {
+    mapModal.style.display = "block";
+
+    if (!map) {
+        map = L.map('map').setView([51.505, -0.09], 2);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        var marker;
+        map.on('click', function(e) {
+            if (marker) {
+                map.removeLayer(marker);
+            }
+            marker = L.marker(e.latlng).addTo(map);
+            document.getElementById('latitude').value = e.latlng.lat;
+            document.getElementById('longitude').value = e.latlng.lng;
+            closeMap();  // Automatically close the map after selecting a location
+        });
+    }
+    setTimeout(function() {
+        map.invalidateSize(); // Fix map rendering issue when opening modal
+    }, 100);
+};
+
+closeMapButton.onclick = function() {
+    closeMap();
+};
+
+function closeMap() {
+    mapModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == mapModal) {
+        closeMap();
+    }
+}
